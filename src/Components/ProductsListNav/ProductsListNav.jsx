3e81dfaa4683bf37e99153061de8e1d02dee2e44
Nguyen Trung Duc <ducnt@lifull-tech.vn>
2023-08-24 15:53:32 +0700

    Add handle form register

--- src/Components/ProductsListNav/ProductsListNav.jsx
+++ src/Components/ProductsListNav/ProductsListNav.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./ProductsListNav.scss";
 
-function ProductsListIndex() {
+function ProductsListNav() {
+  const param = useParams();
+  const men = {
+    desc: "The evolution of an aesthetic narrative. Delve into a collection of shared principles and concepts, which are at once modern yet timeless, for effortless essentials designed with every day in mind.",
+    shoesImg: "https://www.pedroshoes.com/on/demandware.static/-/Library-Sites-Pedro/default/dwd5aa3782/images/plpbanners/pm/aug23/men-shoes.jpg",
+    shoesLink: "./catalog/men/shoes",
+    bagsImg: "https://www.pedroshoes.com/on/demandware.static/-/Library-Sites-Pedro/default/dwa13f95e0/images/plpbanners/pm/aug23/men-bags.jpg",
+    bagsLink: "./catalog/men/bags",
+    accessoriesImg: "https://www.pedroshoes.com/on/demandware.static/-/Library-Sites-Pedro/default/dw0722b854/images/plpbanners/pm/aug23/men-accessories.jpg",
+    accessoriesLink: "./catalog/men/accessories", 
+  };
   return (
-    <div>
-      <h2>VIEW'S ALL</h2>
+    <div className="products-list-nav">
+      {param.type === "new" ? (
+        <div className="new-container">
+          <img
+            src="https://www.pedroshoes.com/on/demandware.static/-/Library-Sites-Pedro/default/dw956c645a/images/plpbanners/plp-slotbanner-desktop.jpg"
+            alt="new products banner"
+          />
+          <div className="new-text">
+            <h2>NEW ARRIVALS</h2>
+          </div>
+        </div>
+      ) : (
+        <div className="type-container">
+          <h2>{param.type.toUpperCase()}'S VIEW ALL</h2>
+          <p>{men.desc}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default ProductsListIndex
+export default ProductsListNav;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,81 +1,228 @@
-import React from 'react'
-import "./Register.scss"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Register.scss";
 
 function Register() {
+  const [users, setUsers] = useState([]);
+  const [userInput, setUserInput] = useState({
+    first: "",
+    last: "",
+    birthday: "",
+    gender: "",
+    contact: "",
+    email: "",
+    password: "",
+    confirmPw: "",
+    newsletter: false,
+    agree: false,
+  });
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setUserInput((userInput) => ({
+      ...userInput,
+      [name]: value,
+    }));
+  };
+  const handleCheck = (e) => {
+    const name = e.target.name;
+    setUserInput((userInput) => ({
+      ...userInput,
+      [name]: !userInput[name],
+    }));
+  };
+  const validateValue = (userInput) => {
+    let errors = {};
+    for (const name in userInput) {
+      if (userInput[name] === "") {
+        errors[name] = "This field is required";
+      }
+    }
+    return errors;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validateValue(userInput));
+    setSubmitting(true);
+  };
+  const submitData = () => {
+    const formData = {
+      first: userInput.first,
+      last: userInput.last,
+      birthday: userInput.birthday,
+      gender: userInput.gender,
+      contact: userInput.contact,
+      email: userInput.email,
+      password: userInput.password,
+      newsletter: userInput.newsletter,
+      agree: userInput.agree,
+    };
+    console.log(formData);
+  };
   return (
     <div className="register-page">
       <div className="form-title">
         <h2>Create An Account</h2>
-        <p>Save your information to checkout faster, save items to your wishlist, and view your purchase and return history.</p>
+        <p>
+          Save your information to checkout faster, save items to your wishlist,
+          and view your purchase and return history.
+        </p>
       </div>
       <div className="reg-form">
         <span>* Mandatory fields are required.</span>
-        <form action="">
+        <form>
           <div className="input-item">
-            <input type="text" name="first" id="first" placeholder='First Name*' />
-            <div className="error">Error</div>
+            <input
+              className={errors.first ? "error-input" : ""}
+              type="text"
+              name="first"
+              id="first"
+              placeholder="First Name*"
+              value={userInput.first}
+              onChange={handleInput}
+            />
+            {errors.first && <div className="error">{errors.first}</div>}
           </div>
           <div className="input-item">
-            <input type="text" name="last" id="last" placeholder='Last Name*' />
-            <div className="error"></div>
+            <input
+              className={errors.last ? "error-input" : ""}
+              type="text"
+              name="last"
+              id="last"
+              placeholder="Last Name*"
+              value={userInput.last}
+              onChange={handleInput}
+            />
+            {errors.last && <div className="error">{errors.last}</div>}
           </div>
           <div className="input-item">
             <div className="dob">
-              <label htmlFor="dob">Date of Birth*:</label>
-              <input type="date" name="dob" id="dob" />
+              <label htmlFor="birthday">Date of Birth*:</label>
+              <input
+                className={errors.birthday ? "error-input" : ""}
+                type="date"
+                name="birthday"
+                id="birthday"
+                value={userInput.birthday}
+                onChange={handleInput}
+              />
             </div>
-            <div className="error"></div>
+            {errors.birthday && <div className="error">{errors.birthday}</div>}
           </div>
           <div className="input-item">
             <div className="genderSelect">
               <label htmlFor="gender">Gender*:</label>
-              <select name="gender" id="gender">
+              <select
+                className={errors.gender ? "error-input" : ""}
+                name="gender"
+                id="gender"
+                value={userInput.gender}
+                onChange={handleInput}
+              >
                 <option value="">Select Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
+                <option name="gender" value="male">
+                  Male
+                </option>
+                <option name="gender" value="female">
+                  Female
+                </option>
               </select>
             </div>
-            <div className="error"></div>
+            {errors.gender && <div className="error">{errors.gender}</div>}
           </div>
           <div className="input-item">
             <div className="phone">
-              <label htmlFor="phone">Contact Number*:</label>
-              <input type="text" name="phone" id="phone" placeholder='Contact Number' />
+              <label htmlFor="contact">Contact Number*:</label>
+              <input
+                className={errors.contact ? "error-input" : ""}
+                type="text"
+                name="contact"
+                id="contact"
+                placeholder="Contact Number"
+                value={userInput.contact}
+                onChange={handleInput}
+              />
             </div>
-            <div className="error"></div>
+            {errors.contact && <div className="error">{errors.contact}</div>}
           </div>
           <div className="input-item">
-            <input type="email" name="email" id="email" placeholder='Email Address*' />
-            <div className="error"></div>
+            <input
+              className={errors.email ? "error-input" : ""}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address*"
+              value={userInput.email}
+              onChange={handleInput}
+            />
+            {errors.email && <div className="error">{errors.email}</div>}
           </div>
           <div className="input-item">
-            <input type="password" name="password" id="password" placeholder='Password*'/>
-            <div className="error"></div>
+            <input
+              className={errors.password ? "error-input" : ""}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password*"
+              value={userInput.password}
+              onChange={handleInput}
+            />
+            {errors.password && <div className="error">{errors.password}</div>}
           </div>
           <div className="input-item">
-            <input type="password" name="confirm" id="confirm" placeholder='Confirm Password*'/>
-            <div className="error"></div>
+            <input
+              className={errors.confirmPw ? "error-input" : ""}
+              type="password"
+              name="confirmPw"
+              id="confirm"
+              placeholder="Confirm Password*"
+              value={userInput.confirmPw}
+              onChange={handleInput}
+            />
+            {errors.confirmPw && (
+              <div className="error">{errors.confirmPw}</div>
+            )}
           </div>
           <div className="input-item">
             <div className="newsletter">
-              <input type="checkbox" name="newsletter" id="newsletter" />
-              <label htmlFor="newsletter">Send me PEDRO new arrival updates, offers, and more.</label>
+              <input
+                type="checkbox"
+                name="newsletter"
+                id="newsletter"
+                checked={userInput.newsletter}
+                onChange={handleCheck}
+              />
+              <label htmlFor="newsletter">
+                Send me PEDRO new arrival updates, offers, and more.
+              </label>
             </div>
             <div className="error"></div>
           </div>
           <div className="input-item">
             <div className="agree">
-              <input type="checkbox" name="agree" id="agree" />
-              <label htmlFor="agree">I agree to PEDRO's <a href=''>Terms & Conditions</a> and <a href=''>Privacy Policy</a></label>
+              <input
+                type="checkbox"
+                name="agree"
+                id="agree"
+                value="agree"
+                checked={userInput.agree}
+                onChange={handleCheck}
+              />
+              <label htmlFor="agree">
+                I agree to PEDRO's <a href="">Terms & Conditions</a> and{" "}
+                <a href="">Privacy Policy</a>
+              </label>
             </div>
+            {errors.agree && <div className="error">{errors.agree}</div>}
           </div>
           <div className="input-item">
-            <button>Create An Account</button>
+            <button onClick={handleSubmit}>Create An Account</button>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
