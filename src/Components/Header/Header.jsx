b73c2ea9adbc0423cca48c8c8501d350f4800008
Nguyen Trung Duc <ducnt@lifull-tech.vn>
2023-09-05 13:22:18 +0700

    get login user to display

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Header.scss";
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,8 +6,18 @@ import { ShoppingOutlined } from '@ant-design/icons';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Header() {
+  // Check if exists loginUser in localStorage and display in Header
+  const [ loginUser, setLoginUser ] = useState({});
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('loginUser'));
+    if (user) {
+      console.log('User exists');
+      setLoginUser(user);
+    } else {
+      console.log('No user logged in');
+    }
+  }, []);
   return (
-    // Check if exists loginUser in localStorage and display in Header
     <div className="p-header">
       <div className="nav">
         <NavLink to={'./'}><img src="https://www.pedroshoes.com/on/demandware.static/Sites-pd-international-Site/-/default/dwcec278f2/images/logo.svg" alt="pedro-logo" /></NavLink>
@@ -20,10 +30,9 @@ function Header() {
       <div className="nav">
         <FontAwesomeIcon icon={icon({name: 'magnifying-glass'})} className="nav-icon" size="lg"/>
         <div className="divider"></div>
-        <NavLink to={'./login'}>
-          <FontAwesomeIcon icon={icon({name: 'user', style: 'regular'})} className="nav-icon" size="lg"/>
-        </NavLink>
+        <FontAwesomeIcon icon={icon({name: 'user', style: 'regular'})} className="nav-icon" size="lg"/>
         {/* Example: display loginUser email here if exist */}
+        {loginUser && <div>{loginUser.email}</div>}
         <div className="divider"></div>
         <div className="shopping-cart">
           <ShoppingOutlined className="nav-icon"/>
